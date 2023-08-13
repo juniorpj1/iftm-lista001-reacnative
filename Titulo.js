5690a01538a26ae63cb3e699d940aeba86ac9a6b
Aparicio Junior <apariciojunior11@gmail.com>
2023-08-13 19:48:56 -0300

    teste

--- Titulo.js
+++ Titulo.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 const Titulo = ({texto1}) => {
   return (
   <View>
-    <Text style={{paddingTop: 50, fontSize: 50, fontWeight: 'bold'}}> {texto1}</Text>
+    <Text style={{paddingTop: 50, fontSize: 50, fontWeight: 'bold'}}> {texto1} </Text>
   </View>
   );
 }
